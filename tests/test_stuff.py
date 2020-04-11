97e41d1202556ac3c25b57cd16f21699e73a694b
zach romer <romerzs14@gmail.com>
2020-04-11 14:06:59 -0700

    Updating debugger pics

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -5,6 +5,9 @@ def test_a_bit():
     import random
     a = 1
     b = 2
-
-    c = random.randint(0,4)
-    assert c > 2
+    di = {'a': [1, 2, 3],
+          'b': {'c': [1, 3, 4],
+                'd': {}},
+          'c': 4}
+    rand = random.randint(0, 4)
+    assert rand > 2
